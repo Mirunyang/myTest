7bf9c43bf39a175e13d437c3328b716d050f0632
Mirunyang <1530804304@qq.com>
2019-05-16 16:03:33 +0800

    添加新的函数

--- src/com/myPackage/Myclass.java
+++ src/com/myPackage/Myclass.java
@@ -2,14 +2,95 @@ package com.myPackage;
 
 public class Myclass {
 
+	 public int myAtoi(String str) {
+	        
+		 if(str.length()==0)
+		 {
+			 return 0;
+		 }
+		 char ch[]=str.toCharArray();
+		 
+		 int num=0;
+		 boolean Ispostive=true;
+		boolean Oreadyhas=false;
+		boolean isfirstZero=true;
+		int index=0;
+		while(index<ch.length)
+		{
+			if(ch[index]==' ')
+			{
+				index++;
+				continue;
+			}
+			else
+			{
+				break;
+			}
+		}
+		 for(int i=index;i<ch.length;i++)
+		 {
+			 if(ch[i]==' ')
+			 {
+				 break;
+			 }
+			/* if(ch[i]=='0'&&isfirstZero&&num==0)
+			 {
+				 continue;
+			 }*/
+			 
+			 if(ch[i]=='-')
+			 {
+				 if(Oreadyhas==true)
+				 {
+					 break;
+				 }
+				Ispostive=false;
+				Oreadyhas=true;
+				continue;
+			 }else if(ch[i]=='+')
+			 {
+				 if(Oreadyhas==true)
+				 {
+					 break;
+				 }
+				 Ispostive=true;
+				 Oreadyhas=true;
+				 continue;
+			 }//-2019
+			  if(ch[i]<='9'&&ch[i]>='0')
+			 {
+				  try
+				  {
+					  isfirstZero=false;
+					  Oreadyhas=true;
+				  int mul=Math.multiplyExact(num, 10);
+				  num=Math.addExact(mul, ch[i]-48);
+			///	num=num*10+ch[i]-48;
+				  }catch(ArithmeticException e)
+				  {
+					  if(Ispostive)
+					  {
+						  return Integer.MAX_VALUE;
+					  }
+					  return Integer.MIN_VALUE;
+				  }
+			 }else
+			 {
+				 break;
+			 }
+		 }
+		 if(Ispostive)
+		 {
+			 return num;
+		 }
+		 return num*(-1);
+	    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		System.out.println("sss");
-		System.out.println("first one");
-		System.out.println("sasasasaassaassa");
-		int t=0;
-		int j=0;
+		Myclass m=new Myclass();
+        int i=m.myAtoi("0-5");
+	    System.out.println(i);
 	}
 
 }
